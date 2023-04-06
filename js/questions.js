8fb0e64796a8d92063994cc5f939bef40f75ec85
Carlos Olvera <carlosolvera@Carloss-Air.home>
2023-04-06 09:03:01 -0400

    create a repo, added current code

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Multi Langauge",
+            "Hyper Text Markup Language",
+            "Hyper Text Maintenance Load",
+            "Hyper Text Multiple Langauge"
+                 ]
+    },
+     {
+        numb: 2, 
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [ 
+            "Hypertext Preprocessor",
+            "Hypertext Preprogramming",
+            "Hypertext Programming",
+            "Hypertext Prolog"
+                 ]
+     },
+     {
+        numb: 3, 
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Statement Question Langauge",
+            "Structured Query Language",
+            "Sorted Question Login",
+            "Stylesheet Query Langauge"
+                 ]
+     },
+     {
+     numb: 4, 
+     question: "What does XML stand for?",
+     answer: "eXtensible Markeup Langauge",
+     options: [
+            "eXamine Madeup Langauge",
+            "eXtensible Markeup Langauge",
+            "eXtra Muliple Langauge",
+            "eXtensive Markup Langauge"
+             ]
+    }, 
+    {
+        numb: 5, 
+        question: "How many assignments can you miss in this class?",
+        answer: "2",
+        options: [
+            "none",
+            "2",
+            "3",
+            "4"
+                ]
+   },
+]
